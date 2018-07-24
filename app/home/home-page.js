8b2b5370184d1464c416bb54c0e89c764d6a6303
Plamen Petkov <plamen.petkov@paysafe.com>
2018-07-24 20:13:41 +0300

    code from live session

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -1,8 +1,19 @@
 var HomeViewModel = require("./home-view-model").HomeViewModel;
+let applicationSettings = require("application-settings")
+let Frame = require("ui/frame")
+
 
 function onNavigatingTo(args) {
     var page = args.object;
     page.bindingContext = new HomeViewModel();
 }
+function logout (args) {
+    applicationSettings.clear()
+    Frame.topmost().navigate({
+        moduleName: "login/login-page",
+        clearHistory: true
+    })
+}
 
+exports.logout = logout;
 exports.onNavigatingTo = onNavigatingTo;
